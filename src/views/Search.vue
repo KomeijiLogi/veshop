14420b47f55bff9abe86efe6fb7f8f69974ca8eb
KomeijiLogi <zclxuan2012@163.com>
2017-12-20 10:49:23 +0800

    调整搜索页面

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -6,11 +6,7 @@
      <div>
        <mt-search v-model="search"  cancel-text="取消" placeholder="请输入要搜索的内容"
        >
-          <mt-cell v-for="k in result" :title="k.title">
-             <router-link :to="{name:'详情页'}">
-                <span>{{k.title}}</span>
-             </router-link>
-          </mt-cell>
+
 
        </mt-search>
      </div>
@@ -34,22 +30,13 @@
         data(){
            return {
               search:'',
-              result:''
+
            }
         },
         computed:{
 
-        },
-        created(){
-          this.$api({
-            url:'/category',
-            method:'post'
-          }).then((res)=>{
-            this.result=res.data;
-          }).catch((error)=>{
-            console.log(error);
-          });
         }
+
 
 
 
